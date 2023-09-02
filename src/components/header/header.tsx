f35a718ee94b0d912ab461f280d8a57c131ea1fa
100peck <stopkadev@gmail.com>
2023-09-02 14:24:47 +0200

    Fix styling on hero/header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import {Menu} from "../menu/menu";
 
 export const Header = () => <header className="w-full bg-transparent">
     <nav
-        className="relative flex w-full items-center justify-between py-2 text-white md:flex-wrap md:justify-start font-mono">
+        className="relative flex w-full items-center justify-between py-2 text-white text-sm md:text-lg md:flex-wrap md:justify-start font-mono">
         <Menu/>
     </nav>
 </header>;
--- src/components/hero/heroContent.tsx
+++ src/components/hero/heroContent.tsx
@@ -9,12 +9,16 @@ export const HeroContent = () => {
 
     const link = '#služby';
 
-    return <div className="flex items-center h-screen xl:w-2/3 md:w-full m-auto ">
-        <div className="py-8 px-4 mx-auto max-w-screen-2xl text-center lg:py-16 lg:px-12">
-            <h1 className="mb-4 text-6xl font-serif tracking-tight leading-none text-gray-900 lg:text-8xl dark:text-white uppercase">Krejčovství a šicí dílna</h1>
-            <h2 className="text-2xl font-mono uppercase">Bobu originals s.r.o.</h2>
+    return <div className="flex h-screen w-full mx-auto ">
+        <div className="md:mt-40 mt-20 py-8 px-4 mx-auto max-w-screen-2xl text-center lg:py-16 lg:px-12">
+            <div className="mb-20">
+                <h1 className="mb-4 text-4xl font-serif tracking-tight leading-none text-gray-900 lg:text-8xl dark:text-white uppercase">Krejčovství
+                    a šicí dílna</h1>
+                <h2 className="lg:text-2xl text-xl font-mono uppercase">Bobu originals s.r.o.</h2>
+            </div>
 
-            <div className="flex flex-col mt-8 mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <div
+                className="flex flex-col mt-8 mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                 <Button text="Co umíme" link={link} type="primary"/>
             </div>
         </div>
