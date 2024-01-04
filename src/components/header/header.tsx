d5f4920fe76bf1195a61f08dbd2f432749bfacb6
100peck <stopkadev@gmail.com>
2024-01-04 15:45:19 +0100

    Add logo to header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,5 +5,6 @@ export const Header = () => <header className="w-full bg-transparent">
     <nav
         className="relative flex w-full items-center justify-between py-2 text-white md:flex-wrap md:justify-start font-mono">
         <Menu/>
+        <div><img src='img/eu-logo-white.png' alt="logo spolufinancováno evropskou unií" className="w-60 m-auto"/></div>
     </nav>
 </header>;
