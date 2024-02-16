7e7d5b55e4c556eab862dde9a3ac721c771529d8
100peck <stopkadev@gmail.com>
2024-02-16 13:18:42 +0100

    Small UI fixes, adding links to the menu.

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,18 +1,20 @@
 import React from "react";
+import {MenuLinks} from '../types.ts'
+
+const {ABOUT, COLLECTION, SERVICES} = MenuLinks;
 
 
 const menuItems = [
-    {title: "Služby", link: "#!"},
-    {title: "Kolekce", link: "#!"},
-    {title: "O nás", link: "#!"},
-    {title: "Naše tvorba", link: "#!"}
+    {title: "Služby", link: `#${SERVICES}`},
+    {title: "Kolekce", link: `#${COLLECTION}`},
+    {title: "O nás", link: `#${ABOUT}`},
 ]
 export const Menu = () => {
     return <ul className="mr-auto px-4 flex flex-row" data-te-navbar-nav-ref>
         {menuItems.map((item) => {
             return <li className="mb-4 pr-2" data-te-nav-item-ref key={item.title}>
                 <a
-                    className="block transition duration-150 ease-in-out hover:text-black-700 focus:text-black-700 disabled:text-black/30 dark:hover:text-black/80 dark:focus:text-black/80 p-2 [&.active]:text-white uppercase"
+                    className="block transition duration-150 ease-in-out hover:underline hover: underline-offset-4 focus:underline focus:underline-offset-4 disabled:text-black/30 p-2 [&.active]:text-white uppercase"
                     href={item.link}
                     data-te-nav-link-ref
                     data-te-ripple-init
--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 type Props = {
+    id?: string;
     title: string;
     children: React.ReactNode;
 }
-export const Section = ({title, children}: Props) => {
-    return <div className="w-full bg-white px-7 py-14 lg:px-14 lg:py-24 text-black">
+export const Section = ({title, id, children}: Props) => {
+    return <div id={id} className="w-full bg-white px-7 py-14 lg:px-14 lg:py-24 text-black">
         <h1 className="text-6xl text-center font-serif mb-10">{title}</h1>
         {children}
     </div>
