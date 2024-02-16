933c792eb92b84baceef2f96cf2d950911e6d21e
100peck <stopkadev@gmail.com>
2024-02-16 13:51:19 +0100

    Set footer color to white explicitly.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -15,13 +15,13 @@ export const Footer = () => {
     return <div id={MenuLinks.FOOTER} className="w-full bg-black p-24">
         <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-10">
             <div>
-                <div className="text-xl text-left mb-6 font-bold font-mono uppercase">Služby</div>
+                <div className="text-xl text-left mb-6 font-bold text-white font-mono uppercase">Služby</div>
                 <ul className="leading-8">
                     {services.map(service => <li key={service.name}>{service.name}</li>)}
                 </ul>
             </div>
             <div>
-                <div className="text-xl text-left mb-6 font-bold font-mono uppercase">Kontakt</div>
+                <div className="text-xl text-left mb-6 font-bold text-white font-mono uppercase">Kontakt</div>
                 <ul className="leading-8">
                     <li>Bobu originals, s.r.o.</li>
                     <li>Třída T.G.Masaryka 1108, Frýdek-Místek</li>
